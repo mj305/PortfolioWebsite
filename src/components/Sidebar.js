2fbdeac48dd73796ff1ef7bff74849f9625c1831
mj305 <maria.j.beckles@gmail.com>
2020-04-18 20:28:09 -0400

    removed Nav bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Nav from './Nav';
 import { Link } from 'gatsby';
+
 export default function SideBar({ fullMenu }) {
   const [headerOpen, toggleHeader] = useState(false);
   return (
@@ -9,23 +9,7 @@ export default function SideBar({ fullMenu }) {
         <h1>
           <Link to="/">&lt; MB / &gt;</Link>
         </h1>
-
-        <nav>
-          <a
-            href="#menu"
-            onClick={e => {
-              e.preventDefault();
-              toggleHeader(!headerOpen);
-            }}
-            className="menuToggle"
-          >
-            <span></span>
-          </a>
-        </nav>
       </header>
-      <div className={`${headerOpen ? 'is-menu-visible' : ' '}`}>
-        <Nav onMenuToggle={() => toggleHeader(!headerOpen)} />
-      </div>
     </>
   );
 }
