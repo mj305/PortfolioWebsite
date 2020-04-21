0365ffa7770d80927d2821262c4f9de831872b87
mj305 <maria.j.beckles@gmail.com>
2020-04-21 17:05:57 -0400

    uploaded most recent resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,38 +35,29 @@ const IndexPage = () => (
           
             <p>
             <span className="image left"><img src={aboutpic} alt="" /></span>
-                I'm a multi-cultural leader and process improvement profesional with 
-                over 16 years of experience in management, operations, recruitment, 
-                project management, and more. I’m a people-person with 
-                deep emotions and empathy; I’m able to inspire and be at 
-                my best when I’m colaborating  with others. 
+            I'm a multicultural leader and process improvement professional with 
+            over 16 years of experience in management, operations, recruitment, 
+            project management, and more. I’m a people-person with deep emotions 
+            and empathy; I’m able to inspire and be at my best when I’m collaborating 
+            with others.
             </p>
 
             <p>
-                I don’t like to define myself by the work I’ve done. 
-                I define myself by the work I want to do. Skills can 
-                be taught, personality is inherent. I prefer to keep 
-                learning, continue challenging myself, and do 
-                interesting things that matter.
-            </p>
-
-            <p>
-                Fueled by high energy levels and boundless enthusiasm, 
-                I’m easily inspired and willing to follow my 
-                fascinations wherever they take me. I’m passionate, 
-                expressive, multi-talented spirit with a natural ability 
-                to entertain and inspire. I’m never satisfied to just come 
-                up with ideas. Instead I have an almost impulsive need 
-                to act on them.
+            I don’t like to define myself by the work I’ve done. I define myself by the 
+            work I want to do. Skills can be taught, personality is inherent. I prefer 
+            to keep learning, continue challenging myself, and do interesting things that 
+            matter. Fueled by high energy levels and boundless enthusiasm, I’m easily 
+            inspired and willing to follow my fascinations wherever they take me. I'm a
+             passionate, expressive, multi-talented spirit with a natural ability to entertain 
+             and inspire. I’m never satisfied to just come up with ideas. Instead I have an 
+             almost impulsive need to act on them.
             </p>
             
             <p>
-                My abundant energy fuels me in the pursuit of many interests, 
-                hobbies, areas of study and artistic endeavors. I’m a fast 
-                learner, able to pick up new skills and juggle different 
-                projects and roles with relative ease. I like to develop 
-                expertise in a number of areas over the course of my life 
-                and career.
+            My abundant energy fuels me in the pursuit of many interests, hobbies, areas of study 
+            and artistic endeavors. I’m a fast learner, able to pick up new skills and juggle 
+            different projects and roles with relative ease. I like to develop expertise in a 
+            number of areas over the course of my life and career.
             </p>
 
           </div>
